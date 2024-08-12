2094958ac179ef724820a6a2c2f417d797009445
clickclown672 <sunfin7@gmail.com>
2024-08-13 01:31:48 +0530

    Login-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
     declarations:[AppComponent],
-    imports:[BrowserModule],
+    imports:[BrowserModule,AppRoutingModule,ReactiveFormsModule],
     bootstrap:[AppComponent]
 })
 
--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',    
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  loginForm!: FormGroup;  // Definite assignment assertion
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      console.log('Login Data:', { email, password });
+      // Implement authentication logic here
+    }
+  }
 }
